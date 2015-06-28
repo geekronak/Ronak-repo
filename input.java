46243f06446b3563cf780a451fa9f39cbf2a433e
ronakdhariya <ronak.dharaiya@gmail.com>
2015-06-28 15:55:39 +0530

    first prog of java
    scanner class to input

---
+++ input.java
@@ -0,0 +1,17 @@
+// Get 10 numbers from user , again get any one num from user and check that num whether it is present or not.If it so return number location
+// WAP to get any size of numbers from user and find prime numbers.  
+
+class input {
+	int i;
+	int ar = new int[10];
+	
+	Scannner scn = new Scanner(System.in); // scanner class for user input
+	
+	for(int i =0;i<int[10];i++){
+		ar[i] = scn.nextInt();
+	}
+	
+	System.out.println("Enter any number " );
+	int input_number = scn.nextInt();
+	System.out.println("Entered number " +input_number);
+} 
